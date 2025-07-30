e40944fa7a830b9c3d345f1e4ffdd99c90041cf6
asiak <joanna.kopec.business@gmail.com>
2025-07-30 12:52:20 +0200

    Drawing a player

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,14 +1,18 @@
 # this allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import * 
+from player import Player
 
 def main():
-    pygame.init()
+    pygame.init() # Pygame initialization
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Creating game window
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    clock = pygame.time.Clock() # Creating clock to control FPS
+
+    dt = 0 # Time before frames
 
     while True: 
         for event in pygame.event.get():
@@ -16,7 +20,10 @@ def main():
                 return # Exits the game loop if window is closed
 
         screen.fill((0, 0, 0)) # Fill screen with black 
+        player.draw(screen)
+
         pygame.display.flip() # Update the display
+        dt = clock.tick(60) / 1000 # Limit the framerate to 60 FPS and calculating delta to from ms to seconds
 
 if __name__ == "__main__":
     main()
